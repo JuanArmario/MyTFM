2107239d6f67573b6d2a0cb73ec2443abfe11f47
JuanArmario <juan.armario.munoz@gmail.com>
2025-02-17 14:19:30 -0500

    - Added JavaScript

---
+++ JavaScript/main.js
@@ -0,0 +1,29 @@
+function loadPage(page) {
+    document.getElementById("contentFrame").src = page;
+}
+
+function loadPDF() {
+    document.getElementById("contentFrame").src = "https://drive.google.com/file/d/10qb4tsqmKVnr-QBjcuoPnsRRUPEsPMTB/preview";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".submenu > a").forEach(menu => {
+        menu.addEventListener("click", function (e) {
+            e.preventDefault();
+            let subMenu = this.nextElementSibling;
+            let icon = this.querySelector(".toggle-icon");
+
+            if (subMenu.style.display === "block") {
+                subMenu.style.display = "none";
+                icon.classList.remove("rotated");
+            } else {
+                subMenu.style.display = "block";
+                icon.classList.add("rotated");
+            }
+        });
+    });
+
+    document.querySelector(".menu-toggle").addEventListener("click", function () {
+        document.querySelector(".sidebar").classList.toggle("active");
+    });
+});
